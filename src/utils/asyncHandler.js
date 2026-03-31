255873ba1e81bbe71a987e6a18e91a678ceffcae
ddebnath <d.debnath@smcs.ac.in>
2026-03-31 15:04:46 +0530

    util code discussion

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,27 @@
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((error) =>
+      next(error)
+    );
+  };
+};
+
+export { asyncHandler };
+
+// const asyncHandler = () => {}
+// const asyncHandler = (fn) => () => {}
+// const asyncHandler = (fn) => () => async () => {}
+
+// const asyncHandler = (fn) => {
+//   return async (req, res, next) => {
+//     try {
+//       await fn(req, res, next);
+//     } catch (error) {
+//       res.status(error.code || 500).json({
+//         success: false,
+//         message: error.message,
+//       });
+//       next(error);
+//     }
+//   };
+// };
